584c3df727bb1b7e799c166a00297aa81a2ac821
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-05-11 12:26:16 +0600

    User Token Created

--- index.js
+++ index.js
@@ -3,8 +3,17 @@
 // express require
 const express = require("express");
 
+// cors require
+const cors = require("cors");
+
+// cookie parser require
+const cookieParser = require("cookie-parser");
+
 // mongodb require
 const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// json web token require
+const jwt = require("jsonwebtoken");
 
 // require dotenv
 require("dotenv").config();
@@ -12,8 +21,25 @@ require("dotenv").config();
 // create an app with express
 const app = express();
 
+// middleware for creating routes
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
 // create and app port
 const port = process.env.PORT || 5000;
+
+// create cookie options
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 
 // home route content
 app.get("/", (req, res) => {
@@ -35,10 +61,23 @@ const client = new MongoClient(uri, {
 // connect mongodb client
 const run = async () => {
   try {
-    // await client.connect();
+    await client.connect();
+
+    // authentication user token route
+    app.post("/signin", async (req, res) => {
+      const userEmail = req.body;
+
+      // create a token with user email
+      const userToken = jwt.sign(userEmail, process.env.USER_SECRET_TOKEN, {
+        expiresIn: "20d",
+      });
+      res
+        .cookie("userToken", userToken, cookieOptions)
+        .send({ success: "Success Login." });
+    });
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
